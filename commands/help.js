79f9a688845b9f15df95f4bf823e71f728dd16e7
genovevafossas <gfossas@knights.ucf.edu>
2019-04-04 11:47:07 -0400

    Fixed printing of help message to entirely use the message in the JSON file.

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 const {HelpMessage} = require ("./../dialogue.json");
 
 exports.run = (bot, message, args) => {
-    message.channel.send("The current commands are as follows: \n```" + HelpMessage + "```");
+    message.channel.send(HelpMessage);
 }
